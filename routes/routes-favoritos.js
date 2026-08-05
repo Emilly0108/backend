27c6b2aee38e44580e27550b9535198533ecbe90
Emily0108 <emi.karen08@gmail.com>
2026-08-05 18:30:22 -0300

    enviando professores e favoritos

---
+++ routes/routes-favoritos.js
@@ -0,0 +1,17 @@
+export function favoritosRoutes(server) {
+  server.get('/favoritos', () => {
+   return 'hello word - favoritos'
+})
+
+  server.post('/favoritos', () => {
+    return 'criar professor'
+})
+
+  server.put('/favoritos/:id', () =>{
+    return 'atualizar favoritos'
+  })
+
+  server.delete('/favoritos/:id', () =>{
+    return 'deletar favoritos'
+  })
+}
---
+++ routes/routes-professores.js
@@ -0,0 +1,17 @@
+export function professoresRoutes(server) {
+  server.get('/professores', () => {
+   return 'hello word - professores'
+})
+
+  server.post('/professores', () => {
+    return 'criar professor'
+})
+
+  server.put('/professores/:id', () =>{
+    return 'atualizar professor'
+  })
+
+  server.delete('/professores/:id', () =>{
+    return 'deletar professor'
+  })
+}
