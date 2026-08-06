e811b06d2232a6d9a70006dc1a93fac4f0e4190e
Emily0108 <emi.karen08@gmail.com>
2026-08-06 10:54:20 -0300

    editando professores e favoritos

--- routes/favoritos.js
+++ routes/favoritos.js
@@ -1,17 +1,17 @@
 export function favoritos(server) {
-  server.get('/favoritos', () => {
+  server.get('/favoritos', async (request, reply) => {
    return 'hello word - favoritos'
 })
 
-  server.post('/favoritos', () => {
-    return 'criar professor'
+  server.post('/favoritos', async (request, reply) => {
+    return 'criar favoritos'
 })
 
-  server.put('/favoritos/:id', () =>{
+  server.put('/favoritos:id', async (request, reply) =>{
     return 'atualizar favoritos'
   })
 
-  server.delete('/favoritos/:id', () =>{
+  server.delete('/favoritos:id', async (request, reply) =>{
     return 'deletar favoritos'
   })
 }
--- routes/professores.js
+++ routes/professores.js
@@ -1,17 +1,17 @@
 export function professores(server) {
-  server.get('/professores', () => {
+  server.get('/professores', async (request, reply) => {
    return 'hello word - professores'
 })
 
-  server.post('/professores', () => {
+  server.post('/professores', async (request, reply) => {
     return 'criar professor'
 })
 
-  server.put('/professores/:id', () =>{
+  server.put('/professores:id', async (request, reply)=>{
     return 'atualizar professor'
   })
 
-  server.delete('/professores/:id', () =>{
+  server.delete('/professores:id', async (request, reply) =>{
     return 'deletar professor'
   })
 }
