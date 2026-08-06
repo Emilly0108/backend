83e23ddcdf10bbe98ba49d432a4eb340669dd55f
Bianca Gabriely <bianca.gabriely@escolar.ifrn.edu.br>
2026-08-06 10:12:48 -0300

    criação de materiais.js e disciplinas.js

--- routes/disciplinas.js
+++ routes/disciplinas.js
@@ -1,2 +1,17 @@
-import {fastify} from 'fastify';
+export function disciplinasRoutes(server){
+    server.post('/disciplinas', async(request, reply) => {
+        return 'Hello - Disciplinas'
+    })
 
+    server.get('/disciplinas', async (request,reply)=>{
+        return 'listar - Disciplinas'
+    })
+
+    server.put('/disciplinas:id', async(request, reply)=>{
+        return 'atualizar - Disciplinas'
+    })
+
+    server.delete('/disciplinas:id', async(request, reply) =>{
+        return 'deletar - Disciplinas'
+    })
+}
