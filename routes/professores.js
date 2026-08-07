e3449b3138b0c0a17c06da29aa2675c2c630fa59
Emily0108 <emi.karen08@gmail.com>
2026-08-07 14:33:22 -0300

    POST de professores

--- routes/professores.js
+++ routes/professores.js
@@ -1,10 +1,26 @@
+import {prisma} from "../lib/prisma.ts"
+import bcrypt from 'bcrypt'
+
 export function professores(server) {
   server.get('/professores', async (request, reply) => {
    return 'hello word - professores'
 })
 
   server.post('/professores', async (request, reply) => {
-    return 'criar professor'
+    const {nome, email, senha, tipo} = request.body
+
+    // Criptografa a senha antes de salvar
+  const senhaCriptografada = await bcrypt.hash(senha, 10)
+
+    const professor = await prisma.professor.create({
+      data:{
+        nome,
+        email,
+        senha: senhaCriptografada,
+        tipo
+      }
+    })
+    return reply.status(201).send(professor)
 })
 
   server.put('/professores:id', async (request, reply)=>{
