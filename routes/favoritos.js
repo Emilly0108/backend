516d7e89f9ce291408bedeb4467b58bac2db2a53
Emily0108 <emi.karen08@gmail.com>
2026-08-08 14:16:37 -0300

    POST GET e DELETE de favoritos

--- routes/favoritos.js
+++ routes/favoritos.js
@@ -1,17 +1,43 @@
+import {prisma} from "../lib/prisma.ts"
+
 export function favoritos(server) {
   server.get('/favoritos', async (request, reply) => {
-   return 'hello word - favoritos'
+   const search = request.query.search
+
+  const favoritos = await prisma.favorito.findMany({
+    where: search ? {
+      material: {
+        titulo: { contains: search, mode: 'insensitive' }
+      }
+    } : undefined,
+    include: {
+      material: true // Traz os dados do material filtrado
+    }
+  })
+
+  return reply.send(favoritos)
+
 })
 
   server.post('/favoritos', async (request, reply) => {
-    return 'criar favoritos'
+    const {id_material} = request.body
+
+    const favorito = await prisma.favorito.create({
+    data: {
+      id_material: Number(id_material), 
+      data_salvo: new Date()           
+    }
+  })
+  return reply.status(201).send(favorito)
 })
 
-  server.put('/favoritos:id', async (request, reply) =>{
-    return 'atualizar favoritos'
-  })
-
   server.delete('/favoritos:id', async (request, reply) =>{
-    return 'deletar favoritos'
+    const { id } = request.params
+
+    await prisma.favorito.delete({
+      where: { id_favorito: Number(id) }
+    })
+
+    return reply.status(204).send()
   })
 }
