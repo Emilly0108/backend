900e3260ce7824ea108d2c38953072af7fd8546d
Emily0108 <emi.karen08@gmail.com>
2026-08-27 10:07:46 -0300

    deploy do backend

--- src/server.ts
+++ src/server.ts
@@ -1,60 +1,55 @@
-import fastify from 'fastify';
-import fastifyMultipart from '@fastify/multipart';
-import fastifyJwt from '@fastify/jwt';
-import fastifyStatic from '@fastify/static';
-import fastifyCors from '@fastify/cors';
-import path from 'path';
-import { router } from '../routes/router';
+import fastify from "fastify";
+import fastifyMultipart from "@fastify/multipart";
+import fastifyJwt from "@fastify/jwt";
+import fastifyStatic from "@fastify/static";
+import fastifyCors from "@fastify/cors";
+import path from "path";
+import { router } from "../routes/router";
+
+const port = 3000;
+const host = "RENDER" in process.env ? `0.0.0.0` : `localhost`;
 
 const app = fastify();
 
-
 // CORS
-await app.register(fastifyCors, {
-    origin: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+app.register(fastifyCors, {
+  origin: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 });
-
 
 // JWT
-await app.register(fastifyJwt, {
-    secret: process.env.JWT_SECRET || 'chave_de_emergencia_caso_env_falhe'
+app.register(fastifyJwt, {
+  secret: process.env.JWT_SECRET || "chave_de_emergencia_caso_env_falhe",
 });
 
-await app.register(fastifyMultipart)
+await app.register(fastifyMultipart);
 
-await app.register(fastifyStatic, {
-    root: path.join(__dirname, '../uploads'),
-    prefix: '/uploads/'
-})
+app.register(fastifyStatic, {
+  root: path.join(__dirname, "../uploads"),
+  prefix: "/uploads/",
+});
 
 // AUTENTICAÇÃO
-app.decorate('authenticate', async function(request: any, reply: any) {
-    try {
-        await request.jwtVerify();
-    } catch (erro) {
-        return reply.status(401).send({
-            error: 'Token inválido ou ausente'
-        });
-    }
+app.decorate("authenticate", async function (request: any, reply: any) {
+  try {
+    await request.jwtVerify();
+  } catch (erro) {
+    return reply.status(401).send({
+      error: "Token inválido ou ausente",
+    });
+  }
 });
 
 // ROTAS
-await app.register(router);
-
+app.register(router);
 
 // SERVIDOR
-app.listen(
-    {
-        port: 3333
-    },
-    (err, address) => {
 
-        if (err) {
-            console.log(err);
-            process.exit(1);
-        }
+app.listen({ host: host, port: port }, (err, address) => {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
 
-        console.log(`Servidor rodando em ${address}`);
-    }
-);
+  console.log(`Servidor rodando em ${address}`);
+});
